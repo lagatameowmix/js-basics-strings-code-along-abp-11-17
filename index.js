9a36f21fcf1b3b24c3725d449f47ee94be32ef3b
Sasha V <sashav4@gmail.com>
2017-11-10 06:15:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Sasha';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, S';
+const shortGreeting = `Welcome, ${currentUser}`;
